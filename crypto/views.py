37df005db33394a882f4368ac41d18ee5ea207e0
Saifur <saifurrahmany43@gmail.com>
2018-11-11 20:37:27 +0600

    price data

--- crypto/views.py
+++ crypto/views.py
@@ -4,6 +4,12 @@ import json
 
 
 def home(request):
+    # price data
+    price_req = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,EOS,LTC,"
+                             "XLM,ADA,USDT,MIOTA,TRX&tsyms=USD")
+    price = json.loads(price_req.content)
+
+    # news
     api_req = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_req.content)
-    return render(request, 'home.html', {'api': api})
+    return render(request, 'home.html', {'api': api, 'price': price})

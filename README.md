a3055bdacd06f4129d5d190e453d066e8dfe612d
Saifur <saifurrahmany43@gmail.com>
2018-11-12 00:19:06 +0600

    search

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Information about Cryptocurrency.
+# Cryptocurrency News
+
+Information and news about Cryptocurrency with current value in USD. You can also search to see latest info about your desired cryptocurrency.
+
+
+![1](https://user-images.githubusercontent.com/16235332/48316602-1f12ef80-e610-11e8-8e20-a9c0e5018dc2.PNG)
+
+![2](https://user-images.githubusercontent.com/16235332/48316608-2639fd80-e610-11e8-9db4-0de325f1d130.PNG)
--- crypto/views.py
+++ crypto/views.py
@@ -13,3 +13,16 @@ def home(request):
     api_req = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_req.content)
     return render(request, 'home.html', {'api': api, 'price': price})
+
+
+def prices(request):
+    if request.method == 'POST':
+        quote = request.POST['quote']
+        quote = quote.upper()
+        cur_req = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+        cur = json.loads(cur_req.content)
+        return render(request, 'prices.html', {'quote': quote, 'cur': cur})
+
+    else:
+        error = "Please put something in the search box"
+        return render(request, 'error.html', {'error': error})
